8e63fbfeeb773234953a13a927da29998bed4155
zhyq0826 <zhyq0826@gmail.com>
2016-01-13 13:02:01 +0800

    fix form-test modelName

--- tests/unit/mixins/form-test.js
+++ tests/unit/mixins/form-test.js
@@ -7,13 +7,13 @@ module('Unit | Mixin | form');
 // Replace this with your real tests.
 test('formComponent object init', function(assert) {
     let FormObject = Ember.Object.extend(formComponent);
-    let subject = FormObject.create();
+    let subject = FormObject.create({modelName: 'user'});
     assert.ok(subject);
 });
 
 
 test('godForm object init', function(assert) {
     let GodFormObject = Ember.Object.extend(godForm);
-    let subject = GodFormObject.create();
+    let subject = GodFormObject.create({modelName: 'user'});
     assert.ok(subject);
 });
