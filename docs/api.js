417ded56a0c1ae48a4490f9817c71f79dd75f00f
zhaoyongqiang <zhyq0826@gmail.com>
2018-07-10 23:52:17 +0800

    Site updated: 2018-07-10 23:52:17

---
+++ docs/api.js
@@ -0,0 +1,55 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "ajax",
+        "formComponent",
+        "godForm",
+        "model",
+        "store"
+    ],
+    "modules": [
+        "ajax",
+        "form",
+        "mixins",
+        "model",
+        "pagination",
+        "services",
+        "store"
+    ],
+    "allModules": [
+        {
+            "displayName": "ajax",
+            "name": "ajax",
+            "description": "support ajax request in this module"
+        },
+        {
+            "displayName": "form",
+            "name": "form",
+            "description": "This module has two mixins: godForm and formComponent, they are used to manage form handler"
+        },
+        {
+            "displayName": "mixins",
+            "name": "mixins"
+        },
+        {
+            "displayName": "model",
+            "name": "model",
+            "description": "orm model"
+        },
+        {
+            "displayName": "pagination",
+            "name": "pagination"
+        },
+        {
+            "displayName": "services",
+            "name": "services"
+        },
+        {
+            "displayName": "store",
+            "name": "store",
+            "description": "store servie mange all model"
+        }
+    ],
+    "elements": []
+} };
+});
