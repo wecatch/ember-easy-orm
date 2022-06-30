80404fedcade177e8fd044c7e822b86ca8c72a2d
zhaoyongqiang <zhyq0826@gmail.com>
2022-06-30 21:27:30 +0800

    rm routes

--- tests/dummy/app/ember-easy-orm/route.js
+++
@@ -1,9 +0,0 @@
-import Route from '@ember/routing/route';
-import { service } from '@ember/service';
-
-export default class IndexRoute extends Route {
-  @service store;
-  model() {
-    return [];
-  }
-}
